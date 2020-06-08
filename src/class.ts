f8cc7c35e9bec7ffca03595b7ddcb30b7084e98f
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-08 16:02:28 +0100

    class

---
+++ src/class.ts
@@ -0,0 +1,14 @@
+/*
+Classes allow us to define how objects should look like,
+which data they should hold. Which methods they should have so that we can easily build objects based on these classes.
+And we then called this instances off the classes.
+* */
+
+// class
+class Department {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name
+    }
+}
