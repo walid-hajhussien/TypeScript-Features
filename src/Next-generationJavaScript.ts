fe3275106b129f35a8221f7a1bf08c80a7366554
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-08 10:52:05 +0100

    object & array

--- src/Next-generationJavaScript.ts
+++ src/Next-generationJavaScript.ts
@@ -37,14 +37,27 @@ let personEs52 = {
 }
 
 //-	Default Function Parameters
-let add2 = (a:number , b:number = 2) => {
+let add2 = (a: number, b: number = 2) => {
     return a + b
 };
 
 // spread operator
-let arr1 = [1,2,3]
-let arr2 = [...arr1,4] //[1,2,3,4]
+let arr1 = [1, 2, 3]
+let arr2 = [...arr1, 4] //[1,2,3,4]
 
-let obj1 = {name:'walid'};
-let obj2 = {...obj1,age:50};
+let obj1 = {name: 'walid'};
+let obj2 = {...obj1, age: 50};
 
+// rest parameter
+let add5 = (n1: number, ...numbers: number[]) => {
+    return numbers.reduce((sum, value) => {
+        return sum + value;
+    }, 0)
+}
+
+// object & array
+let array = [1, 2, 3, 4]
+const [v1, v2, ...rest] = array // v1 = 1 , rest = [3,4]
+
+let obj = {firstName: 'walid', firstAge: 25};
+const {firstName:n1, firstAge:a1} = obj;
