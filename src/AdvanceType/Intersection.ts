9fb6fe532728e95a17211a9690f56ae222eae54d
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-14 09:57:13 +0300

    intersection type

---
+++ src/AdvanceType/Intersection.ts
@@ -0,0 +1,19 @@
+//Intersection types allow us to combine our types
+
+// first type
+type EmployeeType = {
+    name: string
+}
+
+// second type
+type EmployeeDateType = {
+    startDate: Date
+}
+
+// Intersection type
+type AdminType = EmployeeType & EmployeeDateType
+
+const employee1: AdminType = {
+    name: 'walid',
+    startDate: new Date()
+} 
